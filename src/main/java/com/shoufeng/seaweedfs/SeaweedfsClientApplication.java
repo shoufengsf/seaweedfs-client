36216552fcbfabb7848747f436ebc1f76404b760
maozhihao <915855132@qq.com>
2019-12-26 16:07:31 +0800

    seaweedfs客户端

---
+++ src/main/java/com/shoufeng/seaweedfs/SeaweedfsClientApplication.java
@@ -0,0 +1,13 @@
+package com.shoufeng.seaweedfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaweedfsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaweedfsClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoufeng/seaweedfs/SeaweedfsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoufeng.seaweedfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SeaweedfsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
